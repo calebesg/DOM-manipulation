eb8198ee2aadee706b6c139562700593f571c064
CalebeSG <calebeguimaraes10@gmail.com>
2022-02-16 17:46:57 -0300

    saved theme in localStore

--- theme-switcher/script.js
+++ theme-switcher/script.js
@@ -1,8 +1,28 @@
 const body = document.querySelector('body');
 const switchButton = document.querySelector('.switch-container');
 
+(function loadThemePreference() {
+  const currentTheme = localStorage.getItem('@theme');
+
+  if (currentTheme === 'dark') {
+    toggleTheme();
+  }
+}());
+
 switchButton.addEventListener('click', toggleTheme);
 
 function toggleTheme() {
-  body.classList.toggle('dark');
+  let updatingTheme = 'light';
+
+  const isDark = body.classList.toggle('dark');
+
+  if (isDark) {
+    updatingTheme = 'dark';
+  }
+
+  store(updatingTheme);
+}
+
+function store(theme) {
+  localStorage.setItem('@theme', theme);
 }
